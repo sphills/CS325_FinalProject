26e9ff0c1fe7dc869de261b007cdb867cc15481b
Sage Phillips <sage242@hotmail.com>
2024-06-29 15:11:29 -0500

    Added requisite functionality to web server, as can be evidenced by the title updating.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,5 @@
+/*const fs= require('fs');
+
 async function getData() {
     const url = "https://api.weather.gov/points/39.7456,-97.0892";
     try {
@@ -7,13 +9,31 @@ async function getData() {
         }
 
         const json = await response.json();
-        // console.log(json.properties.forecast);
         return json.properties.forecast;
     } catch (error) {
         console.error(error.message);
     }
 }
 
+async function getForecast(forecastUrl) {
+    try {
+        const response = await fetch(forecastUrl);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        let forecastString = (`${json.properties.periods[0].name} ${json.properties.periods[0].startTime} ${json.properties.periods[0].temperature} ${json.properties.periods[0].shortForecast}`);
+
+        return json;
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
 getData().then((forecastUrl) => {
-    document.querySelector("#test-class").innerText = forecastUrl;
+    getForecast(forecastUrl);
 });
+//     document.querySelector("#test-class").innerText = `${forecastUrl.name} ${forecastUrl.startTime} ${forecastUrl.temperature} ${forecastUrl.shortForecast}`;
+// });
+*/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,70 @@
+const fs= require('fs');
 var express = require('express');
 var router = express.Router();
+var forecastData;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  async function getData() {
+    const url = "https://api.weather.gov/points/39.7456,-97.0892";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      return json.properties.forecast;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async function getForecast(forecastUrl) {
+    try {
+      const response = await fetch(forecastUrl);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      let forecastString = "";
+      for (let i = 0; i < json.properties.periods.length; i++) {
+        forecastString += `${json.properties.periods[i].name} ${json.properties.periods[i].startTime} ${json.properties.periods[i].temperature} ${json.properties.periods[i].shortForecast}`;
+        if (Number(i) != Number(json.properties.periods.length)) {
+          forecastString += "\n";
+        }
+      }
+
+      fs.writeFile('./test.txt', forecastString, err => {
+        if (err) {
+          console.error(err);
+        } else {
+          // file written successfully
+        }
+      });
+      return json;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  // getData().then((forecastUrl) => {
+  //   getForecast(forecastUrl).then((json) => {
+  //     forecastData = json;
+  //   });
+  // });
+
+  getData().then((forecastUrl) => {
+    return getForecast(forecastUrl)
+  }).then((json) => {
+    return (forecastData = json);
+  }).then((forecastData) => {
+    console.log("data = " + forecastData);
+    res.render('index', { title: forecastData.properties.periods[0].name });
+  });
+
 });
 
 module.exports = router;
